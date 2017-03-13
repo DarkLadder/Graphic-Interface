7d1ac11affeb713a59b352a9ce99e30cb261ad38
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-03-13 22:57:33 +0300

    v2.2

--- src/Event.java
+++ src/Event.java
@@ -6,10 +6,9 @@ import java.awt.event.ActionListener;
  * Created by Dmitrii on 13.03.2017.
  */
 public class Event implements ActionListener {
+
+
     public void actionPerformed(ActionEvent e) {
-    JButton clickedButton= (JButton)e.getSource();
-    String clickedButtonLabel = clickedButton.getText();
-    JOptionPane.showConfirmDialog(null,"You pressed " +
-            clickedButtonLabel,"Just a test", JOptionPane.PLAIN_MESSAGE);
+
     }
 }
